034a2a581e14d8eb26fb847399e9b2fec1084862
willdarkins <will.darkins@adcellerant.com>
2025-06-04 10:38:26 -0700

    refactored faker data to variable object

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,16 +1,20 @@
 import {test, expect} from '@playwright/test'
 import { faker } from '@faker-js/faker'; // Import faker
 
+const user = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: `${faker.internet.username()}_${Date.now()}@test.com`,
+}
+
+const todoText = faker.lorem.sentence({ min: 4, max: 8 });
+
 test("should be able to add a new todo", async({page}) => {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    const email = `${faker.internet.username()}_${Date.now()}@test.com`;
-    const todoText = faker.lorem.sentence({ min: 3, max: 7 });
 
     await page.goto('https://todo.qacart.com/signup')
-    await page.fill("[data-testid=first-name]", firstName)
-    await page.fill("[data-testid=last-name]", lastName)
-    await page.fill("[data-testid=email]", email)
+    await page.fill("[data-testid=first-name]", user.firstName)
+    await page.fill("[data-testid=last-name]", user.lastName)
+    await page.fill("[data-testid=email]", user.email)
     await page.fill("[data-testid=password]", 'Finley2021!')
     await page.fill("[data-testid=confirm-password]", 'Finley2021!')
 
