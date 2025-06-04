cc19f2424402c9403d4652979a0d5f6c9fb1739f
willdarkins <will.darkins@adcellerant.com>
2025-06-04 12:40:25 -0700

    set up response body for adding new tasks

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,39 +1,44 @@
 import {test, expect} from '@playwright/test'
 import { faker } from '@faker-js/faker'; // Import faker
 
-const user = {
-    firstName: faker.person.firstName(),
-    lastName: faker.person.lastName(),
-    email: `${faker.internet.username()}_${Date.now()}@test.com`,
-    password: faker.internet.password({ length: 12, pattern: /[A-Za-z0-9!@#$%^&*()_+-=]/ })
-}
-
 const todoText = faker.lorem.sentence({ min: 4, max: 8 });
 
-test("should be able to add a new todo", async({page, request}) => {
+test("should be able to add a new todo", async({page, request, context}) => {
     const response = await request.post('/api/v1/users/register', {
         data: {
-            first: user.firstName,
-            last: user.lastName,
-            email: user.email,
-            password: user.password
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email:`${faker.internet.username()}_${Date.now()}@test.com`,
+            password: faker.internet.password({ length: 12, pattern: /[A-Za-z0-9!@#$%^&*()_+-=]/ })
         }
     })
-    // await page.goto('/signup')
-    // await page.fill("[data-testid=first-name]", user.firstName)
-    // await page.fill("[data-testid=last-name]", user.lastName)
-    // await page.fill("[data-testid=email]", user.email)
-    // await page.fill("[data-testid=password]", 'Finley2021!')
-    // await page.fill("[data-testid=confirm-password]", 'Finley2021!')
+    const responseBody = await response.json();
+    const access_token = responseBody.access_token;
+    const firstName = responseBody.firstName;
+    const userID = responseBody.userID;
 
-    // await page.click("[data-testid=submit]")
-
-    // const welcomeMessage = page.locator('[data-testid=welcome]')
-    // await expect(welcomeMessage).toBeVisible()
-    await page.click('[data-testid=add]')
+    await context.addCookies([
+        {
+            name: 'firstName',
+            value: firstName,
+            url: 'https://todo.qacart.com'
+        },
+                {
+            name: 'access_token',
+            value: access_token,
+            url: 'https://todo.qacart.com'
+        },
+        {
+            name: 'userID',
+            value: userID,
+            url: 'https://todo.qacart.com' 
+        }
+    ]);
+    await page.goto('/todo');
+    await page.click('[data-testid=add]');
     await page.fill('[data-testid=new-todo]', todoText)
-    await page.click('[data-testid=submit-newTask]')
-    const toDoItem = page.locator('[data-testid=todo-item]')
+    await page.click('[data-testid=submit-newTask]');
+    const toDoItem = page.locator('[data-testid=todo-item]');
     await expect(toDoItem).toHaveText(todoText)
 })
 
