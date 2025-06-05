422a6b481470be8ddae9c59ff60540946af843ba
willdarkins <will.darkins@adcellerant.com>
2025-06-04 20:41:19 -0700

    added todo api action in delete todo test

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -42,29 +42,52 @@ test("should be able to add a new todo", async({page, request, context}) => {
     await expect(toDoItem).toHaveText(todoText)
 })
 
-test ('should be able to delete a new task', async({page}) => {
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.lastName();
-    const email = `${faker.internet.username()}_${Date.now()}@test.com`;
-    const todoText = faker.lorem.sentence({ min: 3, max: 7 });
-
+test ('should be able to delete a new task', async({page, request, context}) => {
     await page.goto('/signup')
-    await page.fill("[data-testid=first-name]", firstName)
-    await page.fill("[data-testid=last-name]", lastName)
-    await page.fill("[data-testid=email]", email)
-    await page.fill("[data-testid=password]", 'Finley2021!')
-    await page.fill("[data-testid=confirm-password]", 'Finley2021!')
+    const response = await request.post('/api/v1/users/register', {
+        data: {
+            firstName: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email:`${faker.internet.username()}_${Date.now()}@test.com`,
+            password: faker.internet.password({ length: 12, pattern: /[A-Za-z0-9!@#$%^&*()_+-=]/ })
+        }
+    })
+    const responseBody = await response.json();
+    const access_token = responseBody.access_token;
+    const firstName = responseBody.firstName;
+    const userID = responseBody.userID;
 
-    await page.click("[data-testid=submit]")
+    await context.addCookies([
+        {
+            name: 'firstName',
+            value: firstName,
+            url: 'https://todo.qacart.com'
+        },
+                {
+            name: 'access_token',
+            value: access_token,
+            url: 'https://todo.qacart.com'
+        },
+        {
+            name: 'userID',
+            value: userID,
+            url: 'https://todo.qacart.com' 
+        }
+    ]);
 
-    const welcomeMessage = page.locator('[data-testid=welcome]')
-    await expect(welcomeMessage).toBeVisible()
-    await page.click('[data-testid=add]')
-    await page.fill('[data-testid=new-todo]', todoText)
-    await page.click('[data-testid=submit-newTask]')
-    const toDoItem = page.locator('[data-testid=todo-item]')
-    await expect(toDoItem).toHaveText(todoText)
+await request.post('/api/v1/tasks', {
+    data: {
+        isCompleted: false,
+        item: 'Eat Breakfast'
+    },
+    headers: {
+        Authorization: `Bearer ${access_token}`
+    }
+})
+    
+    await page.goto('/todo')
 
-    await page.click('[data-testid=delete]')
-    await expect(toDoItem).not.toBeVisible
+    await page.click('[data-testid=delete]');
+    const noTodosMessage = page.locator('[data-testid=no-todos]');
+    await expect(noTodosMessage).toBeVisible();
 })
